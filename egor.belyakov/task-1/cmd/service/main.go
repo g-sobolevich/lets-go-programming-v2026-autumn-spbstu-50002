e641833c0852f0dedbd2df7ace3aac4e5f35deaf
Dadnsall <115447902+Dadnsall@users.noreply.github.com>
2026-09-24 22:54:14 +0300

    [TASK-1] (#7)
    * [TASK-1] Add calculator
    
    * [TASK-1] Downgrade go version to 1.22

---
+++ egor.belyakov/task-1/cmd/service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		firstNumber, secondNumber int
+		operation                 string
+	)
+
+	_, err := fmt.Scan(&firstNumber)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+
+		return
+	}
+
+	_, err = fmt.Scan(&secondNumber)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(firstNumber + secondNumber)
+	case "-":
+		fmt.Println(firstNumber - secondNumber)
+	case "*":
+		fmt.Println(firstNumber * secondNumber)
+	case "/":
+		if secondNumber == 0 {
+			fmt.Println("Division by zero")
+
+			return
+		}
+
+		fmt.Println(firstNumber / secondNumber)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
---
+++ egor.belyakov/task-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/Dadnsall/task-1
+
+go 1.22
